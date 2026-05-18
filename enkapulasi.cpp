ad4aa1f1957f72568447c1b2775687954eed27fa
senteguhr <senteguhr@gmail.com>
2026-05-18 17:52:22 +0700

    class remote

--- enkapulasi.cpp
+++ enkapulasi.cpp
@@ -3,3 +3,11 @@ using namespace std;
 
 class remote.ampu {
 private:
+    string saklarno[5];
+public:
+    void setsaklarno(int i, string value) {
+        saklarno[i] = value;
+    }
+    string getsaklarno(int i) {
+        return saklarno[i];
+    }
