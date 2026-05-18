822aca39fb94030b83997dad2cc1e65f4cd7f432
senteguhr <senteguhr@gmail.com>
2026-05-18 17:39:54 +0700

    selesai ref

--- multiwaris.cpp
+++ multiwaris.cpp
@@ -7,7 +7,7 @@ public:
     orang(int pUmur) :
         umur  (pUmur)
     {
-        cout << "orang dibuat dengan umur" << umur << "\n" endl;
+        cout << "orang dibuat dengan umur" << umur << "\n"<< endl;
     }
 };
 
@@ -41,6 +41,6 @@ public:
 
 int main() {
     budi a(12);
-    
+
     return 0;
 }
