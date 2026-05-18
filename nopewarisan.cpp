817790ead34c92de868d2e7ce4e88d27a9a543d0
senteguhr <senteguhr@gmail.com>
2026-05-18 18:00:21 +0700

    awal

--- nopewarisan.cpp
+++ nopewarisan.cpp
@@ -1,30 +1,3 @@
 #include <iostream>
 using namespace std;
 
-class baseClass final {
-public:
-    virtual void perkenalan() {
-        cout << "Hallo saya Function dari base class";
-    }
-};
-
-class baseClass1 {
-public:
-    virtual void perkenalan() {
-        cout << "Hallo saya Function dari base class";
-    }
-};
-
-class derivedClass : public baseClass1 {
-public:
-    void perkenalan() {
-        cout << "Hallo saya Function dari derived Class";
-    }
-};
-
-int main() {
-    derivedClass a;
-    a.perkenalan();
-
-    return 0;
-}
